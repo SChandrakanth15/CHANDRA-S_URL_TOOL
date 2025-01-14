193ff88c48d358afba24c71d4e4a3714cad47c3a
SChandrakanth15 <schandrakanth15@gmail.com>
2025-01-14 12:40:03 +0530

    first commit

---
+++ src/main/java/org/example/ChandrasUrlTool.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ChandrasUrlTool {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Welcome to the Chandra's URL Tool.");
+        System.out.println("Type 1 to see the source code of the Website.");
+        System.out.println("Type 2 to see the ip address of the website.");
+        System.out.println("Type 3 to get the HTTP response from a website.");
+        System.out.println("Type 4 to get the HTTP response and the ip address in JSON format.");
+        System.out.println("Type 5 for a list of examples URLs that work.");
+String choice;
+        switch (choice){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+            case "6":
+                break;
+
+        }
+    }
+}
---
+++ src/main/java/org/example/IpAddress.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class IpAddress {
+    public static void main(String[] args)
+    {
+        Scanner userInput = new Scanner(System.in);
+        String url;
+        System.out.println("which website's url you want to see ");
+    }
+}
